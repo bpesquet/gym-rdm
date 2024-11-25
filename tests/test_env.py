bd53a620f0467aa7a8b62de948d912642f42ebe6
Baptiste Pesquet <bpesquet@gmail.com>
2024-11-25 16:39:34 +0100

    Test env rendering

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,9 +2,14 @@
 Unit tests for Random Dot Motion environment.
 """
 
+from pathlib import Path
+import matplotlib.pyplot as plt
 import gymnasium as gym
 from gym_rdm.envs import RandomDotMotionEnv, Action
 from gym_rdm import params
+
+EXPECTED_SHAPE = (params.DISPLAY_SIZE, params.DISPLAY_SIZE, 3)
+OUTPUT_DIR = "_output"
 
 
 def test_env_registration():
@@ -16,23 +21,46 @@ def test_env_registration():
 def test_env():
     """Test the RDM environment"""
 
-    expected_obs_shape = (params.DISPLAY_SIZE, params.DISPLAY_SIZE, 3)
-
     env = RandomDotMotionEnv()
     observation, info = env.reset()
-    assert observation.shape == expected_obs_shape
+    assert observation.shape == EXPECTED_SHAPE
     assert info is None
 
     observation, reward, terminated, truncated, info = env.step(Action.WAIT)
-    assert observation.shape == expected_obs_shape
+    assert observation.shape == EXPECTED_SHAPE
     assert reward == 0  # Temporary
     assert terminated is False
     assert truncated is False
     assert info is None
 
     observation, reward, terminated, truncated, info = env.step(Action.DECISION_LEFT)
-    assert observation.shape == expected_obs_shape
+    assert observation.shape == EXPECTED_SHAPE
     assert reward == 0  # Temporary
     assert terminated is True
     assert truncated is False
     assert info is None
+
+
+def test_env_render_human():
+    """Test environment rendering for humans"""
+
+    env = RandomDotMotionEnv(render_mode="human")
+    env.reset()
+    for _ in range(60):
+        env.step(env.action_space.sample())
+    env.close()
+
+
+def test_env_render_rgb():
+    """Test environment rendering as rgb frames"""
+
+    env = RandomDotMotionEnv(render_mode="rgb_array")
+    env.reset()
+    env.step(env.action_space.sample())
+    frame = env.render()
+    assert frame.shape == EXPECTED_SHAPE
+
+    plt.imshow(frame)
+    # Save the frame to disk
+    Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+    plt.savefig(f"{OUTPUT_DIR}/env_frame_rgb.png")
