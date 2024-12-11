95707d3a6f28ff4759e278e590d1a9bd692079f2
Baptiste Pesquet <bpesquet@gmail.com>
2024-12-11 18:25:16 +0100

    Enable type checking when importing this project

--- gym_rdm/envs/__init__.py
+++ gym_rdm/envs/__init__.py
@@ -2,7 +2,7 @@
 Package definition file.
 """
 
-from .env import RandomDotMotionEnv, Action
+from .env import RandomDotMotionEnv, Action, ObsType, InfoType
 
 # https://mypy.readthedocs.io/en/stable/command_line.html#cmdoption-mypy-no-implicit-reexport
-__all__ = ["RandomDotMotionEnv", "Action"]
+__all__ = ["RandomDotMotionEnv", "Action", "ObsType", "InfoType"]
